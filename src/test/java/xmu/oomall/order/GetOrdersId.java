cec477e79b0f1eb76ac7f596e26076581b7da860
Tens <tens444@163.com>
2019-12-10 10:43:37 +0800

    =

--- src/test/java/xmu/oomall/order/GetOrdersId.java
+++ src/test/java/xmu/oomall/order/GetOrdersId.java
@@ -6,20 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import xmu.oomall.domain.Order;
 import xmu.oomall.util.JacksonUtil;
 
 import java.math.BigDecimal;
 import java.net.URI;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GetOrdersId {
-    @Value("http://${host}:${port}/orders/{id}")
+    @Value("http://${host}:${port}/orders/{id}?userId=9")
     String url;
 
     @Autowired
@@ -29,13 +29,17 @@ public class GetOrdersId {
     public void test1() throws Exception{
         /* 登陆 */
         URI uri = new URI(url.replace("{id}", "999"));
+        HttpHeaders httpHeaders = testRestTemplate.headForHeaders(uri);
+        HttpEntity httpEntity = new HttpEntity(httpHeaders);
 
-        String response = testRestTemplate.getForObject(uri, String.class);
+        ResponseEntity<String> response = testRestTemplate.exchange(uri, HttpMethod.GET, httpEntity, String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
 
-        Integer errno = JacksonUtil.parseInteger(response, "errno");
+        String body = response.getBody();
+        Integer errno = JacksonUtil.parseInteger(body, "errno");
         assertEquals(0, errno);
 
-        Order order = JacksonUtil.parseObject(response, "data", Order.class);
+        Order order = JacksonUtil.parseObject(body, "data", Order.class);
         assertEquals(999, order.getId());
         assertEquals(9, order.getUserId());
         assertEquals("12345", order.getOrderSn());
