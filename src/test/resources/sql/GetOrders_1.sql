27004ae7ace873c27593ceaae59a8e636f3feb70
Tens <tens444@163.com>
2019-12-15 01:01:14 +0800

    init

--- src/test/resources/sql/GetOrders_1.sql
+++
@@ -1,5 +0,0 @@
-insert into `order`(
-	id, user_id, order_sn, status, mobile, message, goods_price, freight_price, coupon_price, integral_price, rebate_price, ship_sn, ship_time, confirm_time, end_time, pay_time, gmt_create, gmt_modified, be_deleted
-) values(
-	999, 9, '12345', 5, '13988888888', '请尽快送达', 182.43, 20, 0, 0, 0, '123456789', '2019-12-05 10:42:06.00', '2019-12-08 10:42:06.00', '2019-12-09 10:42:06.00', '2019-12-04 10:42:06.00', '2019-12-04 10:40:00.00', '2019-12-09 10:42:06.00', 0
-)
