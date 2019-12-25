2320990e538c709901ac0735a8e5b4f1c52db03e
26270 <2627073690@qq.com>
2019-12-25 23:13:23 +0800

    Add FreightTest By 3140

--- src/test/java/xmu/oomall/publictest/freight/DefaultFreightTest.java
+++ src/test/java/xmu/oomall/publictest/freight/DefaultFreightTest.java
@@ -7,10 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import xmu.oomall.PublicTestApplication;
+import xmu.oomall.domain.DefaultFreightPo;
 import xmu.oomall.publictest.AdminAccount;
 import xmu.oomall.util.JacksonUtil;
 
+import java.math.BigDecimal;
 import java.net.URI;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,9 +29,6 @@ public class DefaultFreightTest {
     @Autowired
     private AdminAccount adminAccount;
 
-    /**
-     * @author 24320172203141
-     */
     @Test
     public void tc_defaultFreight_001() throws Exception {
         /**
@@ -47,5 +47,95 @@ public class DefaultFreightTest {
         Integer errno = JacksonUtil.parseInteger(body, "errno");
         assertEquals(0, errno);
     }
+
+    @Test
+    public void tc_defaultFreight_002() throws Exception {
+        /**
+         * @author 24320172203140
+         */
+        // 设置请求头部
+
+        DefaultFreightPo defaultFreightPo=new DefaultFreightPo();
+
+        URI uri = new URI(url);
+        HttpHeaders httpHeaders = adminAccount.createHeaderWithToken();
+        HttpEntity <DefaultFreightPo>httpEntity = new HttpEntity<>(defaultFreightPo,httpHeaders);
+
+        BigDecimal Over10Price = new BigDecimal("10.00");
+        BigDecimal Over50Price = new BigDecimal("20.00");
+        BigDecimal Over100Price = new BigDecimal("30.00");
+        BigDecimal Over300Price = new BigDecimal("40.00");
+        BigDecimal ContinueHeavyPrice = new BigDecimal("50.00");
+        BigDecimal FirstHeavyPrice = new BigDecimal("60.00");
+
+
+        defaultFreightPo.setDestination("{\"dest\":[1,2]}");
+        defaultFreightPo.setGmtCreate(LocalDateTime.now());
+        defaultFreightPo.setGmtModified(LocalDateTime.now());
+        defaultFreightPo.setOver50Price(Over50Price);
+        defaultFreightPo.setOver10Price(Over10Price);
+        defaultFreightPo.setOver100Price(Over100Price);
+        defaultFreightPo.setOver300Price(Over300Price);
+        defaultFreightPo.setContinueHeavyPrice(ContinueHeavyPrice);
+        defaultFreightPo.setFirstHeavyPrice(FirstHeavyPrice);
+
+
+
+        // 发出HTTP请求
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, httpEntity, String.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        // 取得响应体
+        String body = response.getBody();
+        Integer errno = JacksonUtil.parseInteger(body, "errno");
+        DefaultFreightPo testDefaultFreightPo = JacksonUtil.parseObject(body, "data",DefaultFreightPo.class);
+        assertEquals(0, errno);
+        assertEquals(defaultFreightPo.getOver100Price(),testDefaultFreightPo.getOver100Price());
+    }
+
+
+    @Test
+    public void tc_defaultFreight_003() throws Exception {
+        /**
+         * @author 24320172203140
+         */
+        // 设置请求头部
+        DefaultFreightPo defaultFreightPo=new DefaultFreightPo();
+
+        URI uri = new URI(url.replace("{id}","1"));
+        HttpHeaders httpHeaders = adminAccount.createHeaderWithToken();
+        HttpEntity<DefaultFreightPo> httpEntity = new HttpEntity<>(defaultFreightPo,httpHeaders);
+
+        BigDecimal Over10Price = new BigDecimal("10.00");
+        BigDecimal Over50Price = new BigDecimal("20.00");
+        BigDecimal Over100Price = new BigDecimal("30.00");
+        BigDecimal Over300Price = new BigDecimal("40.00");
+        BigDecimal ContinueHeavyPrice = new BigDecimal("50.00");
+        BigDecimal FirstHeavyPrice = new BigDecimal("60.00");
+
+        defaultFreightPo.setDestination("{\"dest\":[1,2]}");
+        defaultFreightPo.setGmtModified(LocalDateTime.now());
+        defaultFreightPo.setOver50Price(Over50Price);
+        defaultFreightPo.setOver10Price(Over10Price);
+        defaultFreightPo.setOver100Price(Over100Price);
+        defaultFreightPo.setOver300Price(Over300Price);
+        defaultFreightPo.setContinueHeavyPrice(ContinueHeavyPrice);
+        defaultFreightPo.setFirstHeavyPrice(FirstHeavyPrice);
+
+
+
+        // 发出HTTP请求
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.PUT, httpEntity, String.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        // 取得响应体
+        String body = response.getBody();
+        Integer errno = JacksonUtil.parseInteger(body, "errno");
+        DefaultFreightPo testDefaultFreightPo = JacksonUtil.parseObject(body, "data",DefaultFreightPo.class);
+        assertEquals(0, errno);
+        assertEquals(defaultFreightPo.getOver100Price(),testDefaultFreightPo.getOver100Price());
+
+    }
+
+
+
 
 }
--- src/test/java/xmu/oomall/publictest/freight/DefaultPieceFreightTest.java
+++ src/test/java/xmu/oomall/publictest/freight/DefaultPieceFreightTest.java
@@ -9,10 +9,13 @@ import org.springframework.http.*;
 
 import org.springframework.web.client.RestTemplate;
 import xmu.oomall.PublicTestApplication;
+import xmu.oomall.domain.DefaultPieceFreightPo;
 import xmu.oomall.publictest.AdminAccount;
 import xmu.oomall.util.JacksonUtil;
 
+import java.math.BigDecimal;
 import java.net.URI;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,11 +36,12 @@ public class DefaultPieceFreightTest {
     @Autowired
     private AdminAccount adminAccount;
 
-    /**
-     * @author 24320172203141
-     */
+
     @Test
     public void tc_defaultPieceFreight_001() throws Exception {
+        /**
+         * @author 24320172203141
+         */
         // 设置请求头部
         URI uri = new URI(url);
         HttpHeaders httpHeaders = adminAccount.createHeaderWithToken();
@@ -53,6 +57,73 @@ public class DefaultPieceFreightTest {
 
     }
 
+    @Test
+    public void tc_defaultPieceFreight_002() throws Exception {
+        /**
+         * @author 24320172203140
+         */
+        // 设置请求头部
+
+        DefaultPieceFreightPo defaultPieceFreightPo=new DefaultPieceFreightPo();
+        BigDecimal UnitRate = new BigDecimal("9.00");
+
+        URI uri = new URI(url);
+        HttpHeaders httpHeaders = adminAccount.createHeaderWithToken();
+        HttpEntity<DefaultPieceFreightPo> httpEntity = new HttpEntity<>(defaultPieceFreightPo,httpHeaders);
+
+
+        defaultPieceFreightPo.setDestination("{\"dest\":[1,2]}");
+        defaultPieceFreightPo.setGmtModified(LocalDateTime.now());
+        defaultPieceFreightPo.setGmtCreate(LocalDateTime.now());
+        defaultPieceFreightPo.setRequireTime("次日");
+        defaultPieceFreightPo.setUnitRate(UnitRate);
+
+
+        // 发出HTTP请求
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, httpEntity, String.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        // 取得响应体
+        String body = response.getBody();
+        Integer errno = JacksonUtil.parseInteger(body, "errno");
+        DefaultPieceFreightPo testDefaultPieceFreightPo = JacksonUtil.parseObject(body, "data",DefaultPieceFreightPo.class);
+        assertEquals(0, errno);
+        assertEquals(defaultPieceFreightPo.getUnitRate(),testDefaultPieceFreightPo.getUnitRate());
+
+    }
+
+
+    @Test
+    public void tc_defaultPieceFreight_003() throws Exception {
+        /**
+         * @author 24320172203140
+         */
+        // 设置请求头部
+        DefaultPieceFreightPo defaultPieceFreightPo=new DefaultPieceFreightPo();
+        BigDecimal UnitRate = new BigDecimal("9.00");
+
+        URI uri = new URI(url.replace("{id}","1"));
+        HttpHeaders httpHeaders = adminAccount.createHeaderWithToken();
+        HttpEntity<DefaultPieceFreightPo> httpEntity = new HttpEntity<>(defaultPieceFreightPo,httpHeaders);
+
+
+        defaultPieceFreightPo.setDestination("{\"dest\":[1,2]}");
+        defaultPieceFreightPo.setGmtModified(LocalDateTime.now());
+        defaultPieceFreightPo.setGmtCreate(LocalDateTime.now());
+        defaultPieceFreightPo.setRequireTime("次日");
+        defaultPieceFreightPo.setUnitRate(UnitRate);
+
+
+        // 发出HTTP请求
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.PUT, httpEntity, String.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        // 取得响应体
+        String body = response.getBody();
+        Integer errno = JacksonUtil.parseInteger(body, "errno");
+        DefaultPieceFreightPo testDefaultPieceFreightPo = JacksonUtil.parseObject(body, "data",DefaultPieceFreightPo.class);
+        assertEquals(0, errno);
+        assertEquals(defaultPieceFreightPo.getUnitRate(),testDefaultPieceFreightPo.getUnitRate());
+
+    }
 
 
 
